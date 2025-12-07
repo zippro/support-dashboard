9b41d795a0ad28cece428665dc208983045adff0
zippro <zippro@gmail.com>
2025-12-07 13:12:44 +0300

    fix: disable browser caching for immediate updates

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    headers: async () => {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache',
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0',
+                    },
+                ],
+            },
+        ];
+    },
+};
 module.exports = nextConfig;
